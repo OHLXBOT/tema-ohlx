4de7a4c4fa33e798985ac591477976b5df69517e
OHLXBOT <128596443+OHLXBOT@users.noreply.github.com>
2023-03-23 12:42:11 +0800

    Create sss.sh

---
+++ sss.sh
@@ -0,0 +1,13 @@
+  if [ `which apt` ]; then
+   if ! command -v node -v &> /dev/null
+  then
+    curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+    apt-get install -y nodejs
+  fi
+else
+   echo "Your OS is unsupported"
+fi
+
+if ! command -v yarn -v &> /dev/null
+then
+    npm i -g yarn
